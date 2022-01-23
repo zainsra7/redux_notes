e4c8f3d4e33b9aee0d44a92235d077b8c01db16c
Zain Sra <m.zain.ul.islam@gmail.com>
2022-01-23 19:41:36 +0000

    Implement Counter React component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
 const counter = (state=0, action) => {
   switch(action.type){
-    case "INCREMENT":
+    case INCREMENT:
       return state + 1;
-    case "DECREMENT":
+    case DECREMENT:
       return state - 1;
       default:
         return state;
@@ -18,23 +21,27 @@ const counter = (state=0, action) => {
 }
 const store = createStore(counter);
 
-const render = ()=> document.body.innerText = store.getState();
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+  );
 
-store.subscribe(render);
 
-render(); // render initial state(0)
-
-document.addEventListener('click', ()=> {
-  store.dispatch({type: 'INCREMENT'})
-});
-
-ReactDOM.render(
+const render = () => ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter 
+    value={store.getState()} 
+    onIncrement={() => store.dispatch({type: INCREMENT})} 
+    onDecrement={() => store.dispatch({type: DECREMENT})} />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+store.subscribe(render);
+render();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
