071d7cbbcc76f6190b3733f10c00cec45c966cad
Zain Sra <m.zain.ul.islam@gmail.com>
2022-01-23 18:28:52 +0000

    Writing counter reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const counter = (state=0, action) => {
+  switch(action.type){
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+      default:
+        return state;
+  }
+}
+const store = createStore(counter);
+
+const render = ()=> document.body.innerText = store.getState();
+
+store.subscribe(render);
+
+render(); // render initial state(0)
+
+document.addEventListener('click', ()=> {
+  store.dispatch({type: 'INCREMENT'})
+});
 
 ReactDOM.render(
   <React.StrictMode>
